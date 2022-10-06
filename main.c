ee27657524a5c2e6d5eec1eb0bf9b1d9c225323a
Yevhenii_Starosta <114361631+Dexr0b@users.noreply.github.com>
2022-10-06 16:28:38 +0300

    Add files via upload

--- main.c
+++ main.c
@@ -1,52 +1,87 @@
 #include <stdio.h>
 #include <stdlib.h>
-#define Name "Yevhenii"
-#define LastName "Starosta"
-#define GroupName "RE-21"
-#define Faculty "RTF"
-#define University "Igor Sikorsky Kyiv Polytechnic Institute"
+
 int main()
 {
-    unsigned int semestr;
-    float admissionScore;
-    int Grade1;
-    int Grade2;
-    int Grade3;
-    int Grade4;
-    int Grade5;
-    double averageGrade;
-    unsigned int age;
-    printf("%s %s Enter following data.\n", Name, LastName);
-    printf("Enter semestr:");
-    scanf("%u", &semestr);
-    printf("Enter your admission score:");
-    scanf("%f", &admissionScore);
-    printf("Enter Grade #1:");
-    scanf("%d", &Grade1);
-    printf("Enter Grade #2:");
-    scanf("%d", &Grade2);
-    printf("Enter Grade #3:");
-    scanf("%d", &Grade3);
-    printf("Enter Grade #4:");
-    scanf("%d", &Grade4);
-    printf("Enter Grade #5:");
-    scanf("%d", &Grade5);
-    printf("Enter your age:");
-    scanf("%u", &age);
+    double F, X1, X2, delta; //Оголошення змінних
+    unsigned int N, method;
 
-    averageGrade = (double)(Grade1 + Grade2 + Grade3 + Grade4 + Grade5)/3;
+    printf("Enter method 1 or 2:");
+    scanf("%d", &method);
 
-    system("cls");
-    printf("\n%s", University);
-    printf("\n%s", Faculty);
-    printf("\n%s", GroupName);
-    printf("\n****************************");
-    printf("\nstudent: %s %s", Name, LastName );
-    printf("\nage: %u", age);
-    printf("\nsemestr: %u", semestr);
-    printf("\n============================");
-    printf("\nadmission score: %.1f", admissionScore);
-    printf("\naverage grade = %.2lf", averageGrade);
-    printf("\n");
-    return 0;
+    while(method!= 1 && method!= 2) // Валідація
+        {
+        printf("ERROR! Wrong method. Enter method again (1 or 2) ");
+        scanf("%d", &method);
+    }
+if(method == 1) //Метод 1
+    {
+        printf("\n\rEnter X1:");
+        scanf("%lf", &X1);
+        printf("\n\rEnter X2:");
+        scanf("%lf", &X2);
+        printf("\n\rSelect N:");
+        scanf("%lu", &N);
+
+        while(N < 1)  //Валідація
+        {
+           printf("ERROR! Enter N > 0 \n");
+           printf("\nEnter N:");
+           scanf("%u", &N);
+        }
+
+        printf("\n************************************************");
+        printf("\n*  N  *         X         *        F(X)        *");
+        printf("\n************************************************");
+
+        int i=1; //Переменна
+        delta=(X2-X1)/(N-1);
+        for(i;i<=N;i++)
+        {
+            F = (8 * pow(X1/16 - 4, 3) - 4 * X1 - 12); //Функція
+            printf("\n|%5d|%16.2f|%13.2f|\n",i,X1,F);
+            printf("\n|************************************|");
+            X1 += delta; // X1 == X1 + delta
+            if(i%10 == 0)
+            {
+                printf("\nPress any key to continue...");
+                getch();
+
+            }
+        }
+    }
+if(method == 2) //Метод 2
+    {
+        printf("\nEnter X1:");
+        scanf("%lf", &X1);
+        printf("\nEnter X2:");
+        scanf("%lf", &X2);
+        printf("\nEnter delta:");
+        scanf("%lf", &delta);
+        int i = 1;
+
+        while(delta < 1.0 && delta > (X2-X1) )  //Валідація
+        {
+           printf("ERROR! Enter delta > 0 \n");
+           printf("\nEnter delta:");
+           scanf("%u", &delta);
+        }
+
+        printf("\n************************************************");
+        printf("\n*  N  *         X         *        F(X)        *");
+        printf("\n************************************************");
+        for (int i=1; X1 <= X2; i++)
+            {
+            F = (8 * pow(X1/16 - 4, 3) - 4 * X1 - 12);
+            printf("\n|%5d|%16.2f|%13.2f|\n",i,X1,F);
+            printf("\n|************************************|");
+            X1 += delta; // X1 == X1 + delta
+           }
+        if(i%10 == 0)
+        {
+            printf("\nPress any key to continue...");
+            getch();
+        }
+}
+return 0;
 }
